8c7fdaf0b91bca671ea3617e198b728c63953f8a
wangcan <wangcan@kuainiugroup.com>
2019-11-28 16:44:02 +0800

    Dockerfile调整

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"github.com/AliyunContainerService/log-pilot/pilot"
+	"github.com/coolwcan/log-pilot/pilot"
 	log "github.com/Sirupsen/logrus"
 	"io/ioutil"
 	"os"
